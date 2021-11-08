44d51220d81e729aeb33aea0ae3778f3b5591612
Ryan824 <71734108+Ryan824@users.noreply.github.com>
2021-11-08 11:14:52 -0500

    This is my Measurable code using Interfaces

--- BankAccount.java
+++ BankAccount.java
@@ -1,54 +1,65 @@
 /**
-   A bank account has a balance and a mechanism for
-   applying interest or fees at the end of the month.
-*/
-public class BankAccount
+   A bank account has a balance that can be changed by
+   deposits and withdrawals.
+ */
+public class BankAccount implements Measurable
 {
-   private double balance;
-   
+	private double balance;
 
-   /**
-      Constructs a bank account with zero balance.
-   */
-   public BankAccount()
-   {
-      balance = 0;
-   }
+	/**
+      Constructs a bank account with a zero balance.
+	 */
+	public BankAccount()
+	{
+		balance = 0;
+	}
 
-   /**
-      Makes a deposit into this account.
-      @param amount the amount of the deposit
-   */
-   public void deposit(double amount)
-   {
-      balance = balance + amount;
-   }
+	/**
+      Constructs a bank account with a given balance.
+      @param initialBalance the initial balance
+	 */
+	public BankAccount(double initialBalance)
+	{
+		balance = initialBalance;
+	}
 
-   /**
-      Makes a withdrawal from this account, or charges a penalty if
-      sufficient funds are not available.
-      @param amount the amount of the withdrawal
-   */
-   public void withdraw(double amount)
-   {
-      balance = balance - amount;
-   }
+	/**
+      Deposits money into the bank account.
+      @param amount the amount to deposit
+	 */
+	public void deposit(double amount)
+	{
+		balance = balance + amount;
+	}
 
-   /**
-      Carries out the end of month processing that is appropriate
-      for this account.
-   */
-   public void monthEnd()
-   {
-			   
-   }
+	/**
+      Withdraws money from the bank account.
+      @param amount the amount to withdraw
+	 */
+	public void withdraw(double amount)
+	{
+		balance = balance - amount;
+	}
 
-   /**
-      Gets the current balance of this bank account.
+	/**
+      Gets the current balance of the bank account.
       @return the current balance
-   */
-   public double getBalance()
-   {
-      return balance;
-   }
+	 */
+	public double getBalance()
+	{
+		return balance;
+	}
+
+	/**
+      Adds interest to the bank account.
+      @param rate The percentage rate of interest gained.
+	 */
+	public void addInterest(double rate)
+	{
+		balance = balance + balance * rate / 100;
+	}
+	public double getMeasure()
+	{
+		return balance;
+	}
 }
